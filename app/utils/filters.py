071ef168fe4abd67ca4c544e1fc5f1f6aae455eb
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-02-08 18:07:58 -0800

    successfully tested URL filter

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -1,5 +1,14 @@
 def format_date(date):
     return date.strftime('%m/%d/%y')
 
+# date test
 from datetime import datetime
 print(format_date(datetime.now()))
+
+
+def format_url(url):
+    return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
+
+# url test
+print(format_url('http://google.com/test/'))
+print(format_url('https://www.google.com?q=test'))
